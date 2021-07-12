4fbb6a20a0ec1d5c62445a8bc397ab66009b23ae
Ivan Matas <ivan.matas@arsfutura.co>
2021-07-12 20:57:29 +0200

    add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import '../styles/globals.css'
 import {ThemeProvider} from 'theme-ui'
 import theme from '../ui/theme/index'
+import Head from "next/head";
+
 
 function MyApp({Component, pageProps}) {
     return (
-        <ThemeProvider theme={theme}><Component {...pageProps} /></ThemeProvider>
+        <>
+            <Head>
+                <link rel="shortcut icon" href="/favicon.ico"/>
+                <title>Ivan Matas</title>
+            </Head>
+            <ThemeProvider theme={theme}><Component {...pageProps} /></ThemeProvider>
+        </>
     )
 }
 
